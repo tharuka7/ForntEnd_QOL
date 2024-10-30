8bd549aa91b2734ecc62cef35d276fbbc645192e
tharuka7 <tharukafonseka7@gmail.com>
2024-10-31 03:18:07 +0530

    adding file

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -12,26 +12,28 @@ function LandingPage() {
   };
 
   return (
+
+    
     <div className="landing-page">
-      <h1>Welcome to Quality of Life Prediction</h1>
-      <p>Click below to start your prediction journey!</p>
-      <button onClick={handleButtonClick} className="start-button">
-        Start Prediction
-      </button>
-      <div style={{ border: '1px solid black', padding: '20px', width: '1000px', margin: 'auto', textAlign: 'center' ,marginTop:'20px'}}>
-            <div style={{ display: 'flex', justifyContent: 'space-between', fontWeight: 'bold', marginBottom: '10px' }}>
-                <div>Survey Count: 2609</div>
-                <div>Country: USA</div>
-            </div>
-            <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
-                <div>Target Audience: Asian Americans</div>
-                <div>Location: Austin</div>
-            </div>
-            <h3>Statistics of the Data Set</h3>
-            <div style={{ marginBottom: '10px', width: '1000px' }}>
-                <img width={1000} src={png1} alt="placeholder" />
-            </div>
+        <div style={{ border: '1px solid black', padding: '20px', width: '1000px', margin: 'auto', textAlign: 'center', marginTop: '20px' }}>
+    <div style={{ display: 'flex', justifyContent: 'space-between', fontWeight: 'bold', marginBottom: '10px' }}>
+        <div>Survey Count: 2609</div>
+        <div>Country: USA</div>
+    </div>
+    <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+        <div>Target Audience: Asian Americans</div>
+        <div>Location: Austin</div>
+    </div>
+    <h3>Statistics of the Data Set</h3>
+    
+    <div style={{ display: 'flex', justifyContent: 'space-around', width: '1000px' }}>
+        <div style={{ border: '1px solid black', padding: '10px', marginRight: '10px' }}>
+            <img width={950} src={png1} alt="placeholder" />
         </div>
+       
+    </div>
+</div>
+      
         <div style={{ border: '1px solid black', padding: '20px', width: '1000px', margin: 'auto', textAlign: 'center' ,marginTop:'20px'}}>
             <div style={{ display: 'flex', justifyContent: 'space-between', fontWeight: 'bold', marginBottom: '10px' }}>
                 <div>Survey Count: 2609</div>
